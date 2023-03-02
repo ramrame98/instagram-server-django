899bab08e2645fc98566fa9967008644f3f448a6
ramram <tnfkachzh@naver.com>
2023-03-02 11:04:52 +0900

    login api

--- users/views.py
+++ users/views.py
@@ -16,3 +16,61 @@ class MyInfo(APIView):
     serializer = UserSerializer(user)
     
     return Response(serializer.data)
+  
+# django의 session을 활용한 로그인
+from rest_framework.exceptions import ParseError
+from django.contrib.auth import authenticate, login
+from rest_framework import status
+class Login(APIView):
+  def post(self, request):
+    username = request.data.get("username")
+    password = request.data.get("password")
+    
+    if not username or not password:
+      raise ParseError()
+    
+    user = authenticate(
+      request,
+      username = username,
+      password = password
+    )
+    
+    print(user)
+    
+    if user:
+      login(request, user)
+      return Response(status=status.HTTP_200_OK)
+    else:
+      return Response(status=status.HTTP_403_FORBIDDEN)
+
+# django의 session을 활용한 로그아웃
+from django.contrib.auth import logout
+class Logout(APIView):
+  def post(self, request):
+    logout(request)
+    return Response(status=status.HTTP_200_OK)
+
+import jwt
+from django.conf import settings
+class JWTLogin(APIView):
+  def post(self, request):
+    username = request.data.get("username")
+    password = request.data.get("password")
+    
+    if not username or not password:
+      raise ParseError
+    
+    user = authenticate(
+      request,
+      username=username,
+      password=password
+    )
+    
+    if user:
+      token = jwt.encode(
+        {"id":user.id, "username":user.username},
+        settings.SECRET_KEY,
+        algorithm="HS256"
+      )
+      print(token)
+      return Response({"token":token})
